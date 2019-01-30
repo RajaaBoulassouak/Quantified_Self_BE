e2751ee671a97797ba8348ff2aefd4fee1db81cf
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-01-29 17:40:52 -0700

    Modify knexfile.js/production

--- knexfile.js
+++ knexfile.js
@@ -12,10 +12,10 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: 'postgres://yvooihzyqtlsie:c8ed3ce281da5e0ba9632976630588e8496fc14c08fd8edb155d0451dc50eb87@ec2-107-21-224-76.compute-1.amazonaws.com:5432/dafu7taifspd4f',
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './db/migrations'
     },
     ssl: true
   }
