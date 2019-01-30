5b2c0b85bc9d1f1fcc791b5798cd74f501e38c96
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-01-30 13:42:54 -0700

    Add DELETE request for deleting a food with id

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const database = require('knex')(configuration);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Quantified-Self-BE';
 
 app.post('/api/v1/foods', (request, response) => {
@@ -18,13 +18,30 @@ app.post('/api/v1/foods', (request, response) => {
       .status(422)
       .send({ error: `Expected format: { title: <String>, calories: <Integer> }. You're missing a "${requiredParameter}" property.` });
     }
-  }                          
+  }                           
   database('foods').insert(food, 'id')
   .then(food => {
     response.status(201).json({ id: food[0]})
   })
   .catch(error => {
     response.status(400).json({ error });
+  });
+});
+
+app.delete('/api/v1/foods/:id', (request, response) => {
+  database('foods').where('id', request.params.id).select()
+  .then(foods => {
+    if (!foods.length) {
+      response.status(404).json({ error: `Could not find food with id ${request.params.id}`})
+    } else {
+      database('foods').where('id', request.params.id).delete()
+      .then(foods => {
+        response.status(204);
+      })
+      .catch(error => {
+        response.status(500).json({ error });
+      })
+    }
   });
 });
 
@@ -38,10 +55,22 @@ app.get('/api/v1/foods', (request, response) => {
   });
 });
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log('Express server listening on port', port)
+app.get('/api/v1/foods/:id', (request, response) => {
+  database('foods').where('id', request.params.id).select()
+    .then(foods => {
+      if (foods.length) {
+        response.status(200).json(foods);
+      } else {
+        response.status(404).json({
+          error: `Could not find food with id ${request.params.id}`
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
 });
-// app.listen(app.get('port'), () => {
-//   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
-// });
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
