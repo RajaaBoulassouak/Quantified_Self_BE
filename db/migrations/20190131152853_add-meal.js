18dd7cba15a1eae4ea1aef6c06f138859dc6a007
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-01-31 15:31:26 -0700

    Create meals table and migrate

---
+++ db/migrations/20190131152853_add-meal.js
@@ -0,0 +1,17 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('meals', function(table) {
+      table.increments('id').primary();
+      table.string('type');
+      table.integer('goal_calories').unsigned();
+      
+      table.timestamps(true, true);
+    }),
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('meals')
+  ]);
+};
