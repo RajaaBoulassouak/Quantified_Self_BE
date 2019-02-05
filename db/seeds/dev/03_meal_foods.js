842b16fb4eb4978a9023c4aa07bd8b545a2810c6
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-02-05 16:14:42 -0700

    Set meal_food IDs to restart at 1 when seeding and update meal_food records

--- db/seeds/dev/03_meal_foods.js
+++ db/seeds/dev/03_meal_foods.js
@@ -1,20 +1,18 @@
 exports.seed = function(knex, Promise) {
-  return knex('meal_foods').del() 
-  .then(() => {
-    return Promise.all([
-      knex('meal_foods').insert([
-        { id: 1, meal_id: 1, food_id: 1 },
-        { id: 2, meal_id: 1, food_id: 2 },
-        { id: 3, meal_id: 2, food_id: 1 },
-        { id: 4, meal_id: 2, food_id: 2 },
-        { id: 5, meal_id: 3, food_id: 1 },
-        { id: 6, meal_id: 3, food_id: 2 },
-        { id: 7, meal_id: 4, food_id: 1 },
-        { id: 8, meal_id: 4, food_id: 2 }
-      ])
-      .then(() => console.log('Seeding complete!'))
-      .catch(error => console.log(`Error seeding data: ${error}`))
+  return knex.raw('TRUNCATE meal_foods RESTART IDENTITY CASCADE')
+  .then(function () {
+    return knex('meal_foods').insert([
+      { meal_id: 1, food_id: 1 },
+      { meal_id: 1, food_id: 2 },
+      { meal_id: 2, food_id: 3 },
+      { meal_id: 2, food_id: 4 },
+      { meal_id: 3, food_id: 5 },
+      { meal_id: 3, food_id: 6 },
+      { meal_id: 4, food_id: 7 },
+      { meal_id: 4, food_id: 8 }
     ])
+    .then(() => console.log('Seeding complete!'))
+    .catch(error => console.log(`Error seeding data: ${error}`))
   })
   .catch(error => console.log(`Error seeding data: ${error}`));
 };
