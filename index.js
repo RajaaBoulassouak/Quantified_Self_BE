2dc75fc3173a76433cd18b9ef62820f16c630241
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-02-06 16:21:52 -0700

    Add CORS access block to index.js file

--- index.js
+++ index.js
@@ -7,11 +7,20 @@ const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 const pry = require('pryjs')
 
-app.use(cors())
+// app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Quantified-Self-BE';
+app.use(function (request, response, next) {
+  response.header("Access-Control-Allow-Origin",
+    "*");
+  response.header("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept");
+  response.header("Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS");
+  next();
+});
 
 
 app.post('/api/v1/foods', (request, response) => {
